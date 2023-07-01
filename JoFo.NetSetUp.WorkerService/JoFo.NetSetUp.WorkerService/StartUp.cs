0ceef1877364da3ed5302dc2db5043735a35a8a2
JoFo <github@johannesfortmann.de>
2023-07-01 15:11:09 +0200

    feat!: Add new StartUp class for WorkerService
    This commit introduces a new StartUp class in the JoFo.NetSetUp.WorkerService namespace. This class is responsible for booting up the server, configuring the application, and handling any exceptions during execution. The class also ensures proper logging using Serilog library. This will allow better control and handling during the application lifecycle, more specifically during the start and shutdown of the server.

---
+++ JoFo.NetSetUp.WorkerService/JoFo.NetSetUp.WorkerService/StartUp.cs
@@ -0,0 +1,51 @@
+﻿using JoFo.NetSetUp.Helper.Configurations;
+using JoFo.NetSetUp.Helper.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+
+
+namespace WorkerServiceSetup;
+
+public class StartUp
+{
+    public static void Run(Action<IConfigurationBuilder> config,
+        Action<IHostBuilder, IConfigurationRoot> builder,
+        Action<IHost> app)
+    {
+        StaticLogger.EnsureInitialized();
+        Log.Information("Server Booting Up...");
+        try
+        {
+            var _builder = Host.CreateDefaultBuilder();
+
+            ConfigurationBuilder _config = new ConfigurationBuilder();
+            config(_config);
+            _config.AddConfigurations();
+            _builder.ConfigureAppConfiguration((_, config) => { config = _config; });
+
+            var _configuration = _config.Build();
+
+            _builder.AddSeriLog(_configuration);
+            builder(_builder, _configuration);
+
+            var _app = _builder.Build();
+            app(_app);
+            _app.RunAsync();
+
+            Console.WriteLine("Server Up");
+            Console.ReadLine();
+        }
+        catch (Exception ex) when (!ex.GetType().Name.Equals("StopTheHostException", StringComparison.Ordinal))
+        {
+            StaticLogger.EnsureInitialized();
+            Log.Fatal(ex, "Unhandled exception");
+        }
+        finally
+        {
+            StaticLogger.EnsureInitialized();
+            Log.Information("Server Shutting down...");
+            Log.CloseAndFlush();
+        }
+    }
+}
